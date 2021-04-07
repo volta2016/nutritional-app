707b547218739662796e919b9412985dbb5522e5
Volta <giovoltadesign@gmail.com>
2021-04-06 23:37:42 -0400

    logic FoodDish - FoodDishes

--- src/components/FoodDishes.js
+++ src/components/FoodDishes.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Grid from "@material-ui/core/Grid";
+import FoodDish from "./FoodDish";
+import NoResults from "./NoResults";
 
-const FoodDishes = () => {
-	return (
-		<div>
-			<p>hola soy FoodDishe!</p>
-		</div>
-	);
-};
+const FoodDishes = ({ hits }) => (
+	<Fragment>
+		{hits.length ? (
+			<div className="root">
+				<Grid container spacing={3} justify="center">
+					{hits.map((hit) => {
+						const { uri, label, image, calories, ingredients } = hit.recipe;
+						return (
+							<FoodDish
+								key={uri}
+								label={label}
+								image={image}
+								calories={calories}
+								ingredients={ingredients}
+							/>
+						);
+					})}
+				</Grid>
+			</div>
+		) : (
+			<NoResults />
+		)}
+	</Fragment>
+);
 
 export default FoodDishes;
---
+++ src/components/NoResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+const NoResults = () => (
+	<Grid item xd={12}>
+		<Paper className="paper">
+			<h2>No Results</h2>
+		</Paper>
+	</Grid>
+);
+
+export default NoResults;
